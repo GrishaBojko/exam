9902d63b095a590fb5eb0637338dc7d2fb53c5b0
grishabojko <grishabojko2@gmail.com>
2019-11-02 13:14:08 +0200

    1

---
+++ dist/main.319f712ab1614b045f9d.hot-update.js
@@ -0,0 +1,14 @@
+webpackHotUpdate("main",{
+
+/***/ "./node_modules/file-loader/dist/cjs.js?!./node_modules/extract-loader/lib/extractLoader.js!./node_modules/css-loader/dist/cjs.js?!./node_modules/sass-loader/dist/cjs.js?!./node_modules/postcss-loader/src/index.js?!./src/scss/styles.scss":
+/*!*******************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/file-loader/dist/cjs.js??ref--8-1!./node_modules/extract-loader/lib/extractLoader.js!./node_modules/css-loader/dist/cjs.js??ref--8-3!./node_modules/sass-loader/dist/cjs.js??ref--8-4!./node_modules/postcss-loader/src??ref--8-5!./src/scss/styles.scss ***!
+  \*******************************************************************************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/postcss-loader/src/index.js):\\nSyntaxError\\n\\n(728:5) Unclosed block\\n\\n \\u001b[90m 726 | \\u001b[39m      \\u001b[33m}\\u001b[39m\\n \\u001b[90m 727 | \\u001b[39m    \\u001b[33m}\\u001b[39m\\n\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 728 | \\u001b[39m    \\u001b[33m.choose__gallery\\u001b[39m\\u001b[33m{\\u001b[39m\\n \\u001b[90m     | \\u001b[39m    \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\n \\u001b[90m 729 | \\u001b[39m  \\n \\u001b[90m 730 | \\u001b[39m      margin\\u001b[33m:\\u001b[39m 0\\u001b[33m;\\u001b[39m\\n\");\n\n//# sourceURL=webpack:///./src/scss/styles.scss?./node_modules/file-loader/dist/cjs.js??ref--8-1!./node_modules/extract-loader/lib/extractLoader.js!./node_modules/css-loader/dist/cjs.js??ref--8-3!./node_modules/sass-loader/dist/cjs.js??ref--8-4!./node_modules/postcss-loader/src??ref--8-5");
+
+/***/ })
+
+})
---
+++ dist/main.f2e4d2b2e33cb9df10d2.hot-update.js
@@ -0,0 +1,14 @@
+webpackHotUpdate("main",{
+
+/***/ "./node_modules/file-loader/dist/cjs.js?!./node_modules/extract-loader/lib/extractLoader.js!./node_modules/css-loader/dist/cjs.js?!./node_modules/sass-loader/dist/cjs.js?!./node_modules/postcss-loader/src/index.js?!./src/scss/styles.scss":
+/*!*******************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/file-loader/dist/cjs.js??ref--8-1!./node_modules/extract-loader/lib/extractLoader.js!./node_modules/css-loader/dist/cjs.js??ref--8-3!./node_modules/sass-loader/dist/cjs.js??ref--8-4!./node_modules/postcss-loader/src??ref--8-5!./src/scss/styles.scss ***!
+  \*******************************************************************************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\nSassError: media query expression must begin with '('\\n        on line 14 of C:\\\\Users\\\\otto\\\\Desktop\\\\examnov1\\\\src\\\\scss\\\\styles.scss\\n>> @media screen and  and  (max-width: 4px){\\r\\n   --------------^\\n\");\n\n//# sourceURL=webpack:///./src/scss/styles.scss?./node_modules/file-loader/dist/cjs.js??ref--8-1!./node_modules/extract-loader/lib/extractLoader.js!./node_modules/css-loader/dist/cjs.js??ref--8-3!./node_modules/sass-loader/dist/cjs.js??ref--8-4!./node_modules/postcss-loader/src??ref--8-5");
+
+/***/ })
+
+})
